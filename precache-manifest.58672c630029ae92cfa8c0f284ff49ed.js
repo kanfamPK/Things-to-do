c44fa2ba91b354a0ff6e331af1a4d0d58b5522fa
pkomg <khanpham1995@gmail.com>
2019-09-18 17:30:02 +0700

    Updates

---
+++ precache-manifest.58672c630029ae92cfa8c0f284ff49ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd7c0f66a93a38227dd96ab708e5eeb",
+    "url": "/kanfamPK/Things-to-do/index.html"
+  },
+  {
+    "revision": "a7a5cd1e49c9f53d877b",
+    "url": "/kanfamPK/Things-to-do/static/css/main.f2d02ff1.chunk.css"
+  },
+  {
+    "revision": "d67a3512f50cab9e48de",
+    "url": "/kanfamPK/Things-to-do/static/js/2.767bb848.chunk.js"
+  },
+  {
+    "revision": "a7a5cd1e49c9f53d877b",
+    "url": "/kanfamPK/Things-to-do/static/js/main.37affc90.chunk.js"
+  },
+  {
+    "revision": "f8dc7eacb2292d4aba85",
+    "url": "/kanfamPK/Things-to-do/static/js/runtime~main.38491181.js"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/kanfamPK/Things-to-do/static/media/check-done.d29f1779.svg"
+  },
+  {
+    "revision": "40eb9cc6d51d63d1dfd8a0ea29b8d700",
+    "url": "/kanfamPK/Things-to-do/static/media/check.40eb9cc6.svg"
+  },
+  {
+    "revision": "d51c4cbdf58ed633c094cc393e644e66",
+    "url": "/kanfamPK/Things-to-do/static/media/down-arrow.d51c4cbd.svg"
+  }
+]);
